baba34e167b68137fd33ca0de3dc49b8cd0166b9
Marcos Mendes <marcosdev.me@gmail.com>
2024-11-04 11:14:16 -0300

    feat: add global props

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,12 +1,15 @@
-interface ButtonProps {
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: React.ReactNode;
 }
 
-export const Button = ({ icon }: ButtonProps) => {
+export const Button = ({ icon, ...rest }: ButtonProps) => {
   return (
     <button
       aria-label="Settings"
       className="flex h-10 w-10 cursor-pointer items-center justify-center transition-colors duration-200 hover:opacity-50"
+      {...rest}
     >
       {icon && icon}
     </button>
