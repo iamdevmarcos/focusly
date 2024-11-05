7d5bd95ee5a813dacd0ff76463359aa4d1be6ef7
Marcos Mendes <marcosdev.me@gmail.com>
2024-11-04 21:40:34 -0300

    fix: adjust google fonts api

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100,200,300;400;500;600;700&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;500;600;700&display=swap",
   },
   { rel: "stylesheet", href: "/styles/sonner.css" },
   {
